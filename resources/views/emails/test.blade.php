173fc8d4da158d760b536e881a92599989b62427
Eshanezer <eshan.dilmina.d@outlook.com>
2020-07-30 18:30:15 +0530

    Initial commit

---
+++ resources/views/emails/test.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel</title>
+
+</head>
+<body>
+<div class="container">
+    <div class="content">
+        <h1>{{$title}}</h1>
+        <p>{{$content}}</p>
+    </div>
+</div>
+</body>
+</html>
